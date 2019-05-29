d8bdec245636d9b60f11a7b53812425d3c371cce
Michael Ruf <Ruf_Michael@gmx.de>
2019-05-29 19:42:33 +0200

    WIP

--- docker/chmod.sh
+++ docker/chmod.sh
@@ -1,1 +1,7 @@
+#!/bin/sh
 chmod +x docker-daemon-config.install.sh
+
+chown 2000:2000 /docker/DATA/mail-out/dovecot
+chmod 600 /docker/DATA/mail-out/dovecot
+# Dont know if +x necessary (more likely not)
+chmod +x /docker/DATA/mail-out/dovecot
